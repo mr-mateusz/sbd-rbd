ee22cc544e647e85062be6f58d06c1b075481580
Szymon Bocian <szymon.bocian@student.wat.edu.pl>
2020-05-29 21:57:53 +0200

    Add expilcite table id's for POS

--- generate_insert.py
+++ generate_insert.py
@@ -16,25 +16,22 @@ ascii_letters = string.ascii_letters
 end_line = ","
 
 print("INSERT INTO sale.point_of_sale")
-print("(\n\tpos_code,\n\tstore_id\n)")
+print("(\n\tid,\n\tpos_code,\n\tstore_id\n)")
 print("VALUES")
 
-for i in range(0,n_pos):
-    prefix_code = "".join(choice(upper_letters) for _ in range(0,2))
-    midfix_code = randint(10000,99999)
-    endfix_code = "".join(choice(ascii_letters) for _ in range(0,5))
-    suffix_code = randint(100,999)
-    store_code = randint(1,store_id)
-    
-    if(i==n_pos-1):
+for i in range(0, n_pos):
+    prefix_code = "".join(choice(upper_letters) for _ in range(0, 2))
+    midfix_code = randint(10000, 99999)
+    endfix_code = "".join(choice(ascii_letters) for _ in range(0, 5))
+    suffix_code = randint(100, 999)
+    store_code = randint(1, store_id)
+
+    if(i == n_pos-1):
         end_line = ";"
 
-    print('("_{}-[{}={}]_{}",{}){}'.format(prefix_code,
-                                           midfix_code,
-                                           endfix_code,
-                                           suffix_code,
-                                           store_code,
-                                           end_line))
+    print('({},"_{}-[{}={}]_{}",{}){}'.format(i+1, prefix_code, midfix_code,
+                                              endfix_code, suffix_code, store_code,
+                                              end_line))
 
 end_line = ","
 
@@ -43,22 +40,22 @@ print("INSERT INTO sale.transaction")
 print("(\n\tpos_id,\n\ttransaction_type_id,\n\tproduct_id,\n\ttransaction_date\n)")
 print("VALUES")
 
-for i in range(0,n_transaction):
+for i in range(0, n_transaction):
 
-    i_pos = randint(1,n_pos)
-    i_tran = randint(1,transaction_type_id)
-    i_prod = randint(1,product_id)
+    i_pos = randint(1, n_pos)
+    i_tran = randint(1, transaction_type_id)
+    i_prod = randint(1, product_id)
 
-    year = randint(2019,2020)
-    month = randint(1,12)
-    day = randint(1,31)
+    year = randint(2019, 2020)
+    month = randint(1, 12)
+    day = randint(1, 31)
 
     try:
-        i_date = date(year,month,day)
+        i_date = date(year, month, day)
     except Exception as e:
-        i_date = date(2020,2,28)
+        i_date = date(2020, 2, 28)
 
-    if(i==n_pos-1):
+    if(i == n_pos-1):
         end_line = ";"
 
     print('({},{},{},"{}"){}'.format(i_pos,
